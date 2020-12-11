6f20a84211afd1063619ba5d88bdd49dbfdaed53
Thomas Rohde <trohde@rocketmail.com>
2020-12-11 14:23:01 -0600

    initial commit - set up index, client.js, jquery, style.css, bootstrap documents in project

---
+++ client.js
@@ -0,0 +1,41 @@
+console.log('js is loaded');
+
+
+
+
+
+$(document).ready(handleReady);
+
+
+function handleReady(){
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('end of file');
