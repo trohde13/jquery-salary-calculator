00672458ee358e23320320ab05cdb49783901cdb
Thomas Rohde <trohde@rocketmail.com>
2020-12-12 11:50:35 -0600

    added a getter called newEmployee to collect input info, add it to employees array, & calls renderToDom to append to table

--- client.js
+++ client.js
@@ -15,17 +15,31 @@ function handleReady(){
     //click listeners
     $('#submitButton').on('click', handleClick);
 
-}
+}; //end handleReady
 
 function renderToDom(){
 
 
-}
+}; //end renderToDom
 
 function handleClick(){
     console.log('clicked submit');
 
-}
+    //adds employee info from inputs to employees array
+    let newEmployee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        idIn: $('idIn').val(),
+        titleIn: $('titleIn').val(),
+        annualSalaryIn: $('annualSalaryIn').val(),
+    };
+    console.log(newEmployee); //check input in console
+    employees.push(newEmployee); //pushes newEmployee object to array
+
+    renderToDom(); //calls renderToDom to run function to append to table
+
+
+}; //end handleClick
 
 
 
